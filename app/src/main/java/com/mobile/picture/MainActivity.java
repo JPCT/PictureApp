c1b44305eb1b8a64a4337898883a973c93a0af21
Juan Pablo Castaño Tinoco <jp.tinoco@transportsystems.co>
2022-04-19 11:59:31 -0500

    Init views

--- app/src/main/java/com/mobile/picture/MainActivity.java
+++ app/src/main/java/com/mobile/picture/MainActivity.java
@@ -3,12 +3,31 @@ package com.mobile.picture;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageViewPhoto;
+    private Button btnTakePhoto;
+    private Button btnSelectPhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+        initEvents();
+    }
+
+    public void initViews(){
+        imageViewPhoto = findViewById(R.id.imgPhoto);
+        btnTakePhoto = findViewById(R.id.butTakePhoto);
+        btnSelectPhoto = findViewById(R.id.butSelectPhoto);
+    }
+
+    public void initEvents(){
+
     }
 }
