503c08a6ddfd2a89fa52c4124d0d17bdc0541548
sebitas <juan.1701715778@ucaldas.edu.co>
2022-04-22 11:56:37 -0500

    initEvents completo

--- app/src/main/java/com/mobile/picture/MainActivity.java
+++ app/src/main/java/com/mobile/picture/MainActivity.java
@@ -1,10 +1,21 @@
 package com.mobile.picture;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,6 +39,88 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void initEvents(){
+        String root = getExternalFilesDir(Environment.DIRECTORY_PICTURES).toString();
+        String imageFolderPath = root;
+        File imagesFolder = new File(imageFolderPath);
 
+        if(imagesFolder.mkdirs()){
+            Log.d("Take Photo", imagesFolder + " created.");
+        } else {
+            Log.d("Take Photo", imagesFolder + " NOT created.");
+        }
+        File image = new File(imageFolderPath, "TempPhoto.jpg");
+        image.delete();
+        Uri fileUri = FileProvider.getUriForFile(this, "com.mobile.picture", image);
+        actResLauncherTakePhoto = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+               switch (result.getResultCode()){
+                   case RESULT_OK:
+                       //for (int k = 1; k <= 6; k++) {
+                           try {
+                               final Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), fileUri);
+                               if(bitmap != null) {
+                                   runOnUiThread(() -> imageViewPhoto.setImageURI(fileUri));
+                                   break;
+                               } else {
+                                   Thread.sleep(500);
+                               }
+                           } catch (Exception e){
+                               //e.printStackTrace();
+                           }
+                       //}
+                       break;
+                   case RESULT_CANCELED:
+                       Log.e("Take Photo", "Result Cancel");
+                       break;
+                   default:
+               }
+        });
+
+        btnTakePhoto.setOnClickListener(v -> {
+            if(hasCameraHardware(this)) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+                if (intent.resolveActivity(getPackageManager()) != null){
+                    try {
+                        actResLauncherTakePhoto.launch(intent);
+                    }catch (Exception e) {
+                        Toast.makeText(this, "Permission denied",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+        btnSelectPhoto.setOnClickListener(v -> {
+            Intent intent = new Intent();
+            intent.setType("image/*");
+            intent.setAction(Intent.ACTION_PICK);
+            if(intent.resolveActivity(getPackageManager()) != null) {
+                try {
+                    actResLauncherSelectPhoto.launch(intent);
+                } catch (Exception e) {
+                    Toast.makeText(this, "Permission denied",
+                            Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                Toast.makeText(this, "There is no app that support this action",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        actResLauncherSelectPhoto = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+            Log.e("Take Photo", "YYYYYY");
+            Log.e("Take Photo", "Result = " + result);
+            switch (result.getResultCode()) {
+                case RESULT_OK:
+                    Log.d("Take photo", "Select photo");
+                    Uri selectedImageUri = result.getData().getData();
+                    if(null != selectedImageUri) {
+                        //update the preview image in the layout
+                        runOnUiThread(() -> imageViewPhoto.setImageURI(selectedImageUri));
+                    }
+                    break;
+            }
+        });
     }
 }
