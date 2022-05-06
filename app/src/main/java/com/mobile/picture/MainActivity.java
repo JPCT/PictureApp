26c30c005b2ab880e282ac4aa813c62d5dd74d87
Juan Pablo Castaño Tinoco <jp.tinoco@transportsystems.co>
2022-05-06 16:04:49 -0500

    cambios main

--- app/src/main/java/com/mobile/picture/MainActivity.java
+++ app/src/main/java/com/mobile/picture/MainActivity.java
@@ -1,10 +1,15 @@
 package com.mobile.picture;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
@@ -22,14 +27,43 @@ public class MainActivity extends AppCompatActivity {
     private ImageView imageViewPhoto;
     private Button btnTakePhoto;
     private Button btnSelectPhoto;
+    private Uri fileUri;
+
+    ActivityResultLauncher<Intent> actResLauncherTakePhoto;
+    ActivityResultLauncher<Intent> actResLauncherSelectPhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        String root = getExternalFilesDir(Environment.DIRECTORY_PICTURES).toString();
+
+        String imageFolderPath = root;
+        File imagesFolder = new File(imageFolderPath);
+
+        if (imagesFolder.mkdirs()) {
+            Log.d("Take Photo", imagesFolder + " created.");
+        } else {
+            Log.d("Take Photo", imagesFolder + " NOT created.");
+        }
+
+        File image = new File(imageFolderPath, "TempPhoto.jpg");
+        fileUri = FileProvider.getUriForFile(this, "com.example.demotakephoto", image);
+
         initViews();
         initEvents();
+
+        showPhoto(fileUri);
+    }
+
+    public void showPhoto(Uri fileUri) {
+        try {
+            final Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), fileUri);
+            runOnUiThread(() -> imageViewPhoto.setImageURI(fileUri));
+        } catch (Exception e) {
+            //e.printStackTrace();
+        }
     }
 
     public void initViews(){
@@ -38,56 +72,76 @@ public class MainActivity extends AppCompatActivity {
         btnSelectPhoto = findViewById(R.id.butSelectPhoto);
     }
 
-    public void initEvents(){
-        String root = getExternalFilesDir(Environment.DIRECTORY_PICTURES).toString();
-        String imageFolderPath = root;
-        File imagesFolder = new File(imageFolderPath);
+    public void initEvents() {
+        ActivityResultContracts.StartActivityForResult actForRes = null;
 
-        if(imagesFolder.mkdirs()){
-            Log.d("Take Photo", imagesFolder + " created.");
-        } else {
-            Log.d("Take Photo", imagesFolder + " NOT created.");
-        }
-        File image = new File(imageFolderPath, "TempPhoto.jpg");
-        image.delete();
-        Uri fileUri = FileProvider.getUriForFile(this, "com.mobile.picture", image);
-        actResLauncherTakePhoto = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
-               switch (result.getResultCode()){
-                   case RESULT_OK:
-                       //for (int k = 1; k <= 6; k++) {
-                           try {
-                               final Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), fileUri);
-                               if(bitmap != null) {
-                                   runOnUiThread(() -> imageViewPhoto.setImageURI(fileUri));
-                                   break;
-                               } else {
-                                   Thread.sleep(500);
-                               }
-                           } catch (Exception e){
-                               //e.printStackTrace();
-                           }
-                       //}
-                       break;
-                   case RESULT_CANCELED:
-                       Log.e("Take Photo", "Result Cancel");
-                       break;
-                   default:
-               }
+        // Take photo from camera
+        actForRes = new ActivityResultContracts.StartActivityForResult();
+        actResLauncherTakePhoto = registerForActivityResult(actForRes, result -> {
+            switch (result.getResultCode()) {
+                case RESULT_OK:
+                    showPhoto(fileUri);
+                    break;
+                case RESULT_CANCELED:
+                    //Log.e("Take Photo","Result Cancel");
+                    break;
+                default:
+                    //Log.e("Take Photo", "getResultCode = " + result.getResultCode());
+                    break;
+            }
         });
 
         btnTakePhoto.setOnClickListener(v -> {
-            if(hasCameraHardware(this)) {
+            if (hasCameraHardware(this)) {
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
-                intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
-                if (intent.resolveActivity(getPackageManager()) != null){
-                    try {
-                        actResLauncherTakePhoto.launch(intent);
-                    }catch (Exception e) {
-                        Toast.makeText(this, "Permission denied",
-                                Toast.LENGTH_SHORT).show();
-                    }
+                if (hasCameraPermissions(this)){
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+                    if (intent.resolveActivity(getPackageManager()) != null) {
+                        try {
+                            actResLauncherTakePhoto.launch(intent);
+                        } catch (Exception e) {
+                            AlertDialog.Builder alert = new AlertDialog.Builder(this)
+                                    .setTitle(getString(R.string.Error))
+                                    .setIcon(android.R.drawable.ic_dialog_alert)
+                                    .setMessage(getString(R.string.You_must_enable_permission_camera))
+                                    .setPositiveButton(getString(R.string.OK), null);
+                            alert.show();
+                        }
+                    } else {
+                        AlertDialog.Builder alert = new AlertDialog.Builder(this)
+                                .setTitle(getString(R.string.Error))
+                                .setMessage(getString(R.string.There_is_no_app_that_support_this_action))
+                                .setPositiveButton(getString(R.string.OK), null);
+                        alert.show();
+                    }}
+                else{
+                    AlertDialog.Builder alert = new AlertDialog.Builder(this)
+                            .setTitle(getString(R.string.Error))
+                            .setIcon(android.R.drawable.ic_dialog_alert)
+                            .setMessage(getString(R.string.You_must_enable_permission_camera))
+                            .setPositiveButton(getString(R.string.OK), null);
+                    alert.show();
                 }
+            } else {
+                AlertDialog.Builder alert = new AlertDialog.Builder(this)
+                        .setTitle(getString(R.string.Info))
+                        .setMessage(getString(R.string.Camera_not_detected))
+                        .setPositiveButton(getString(R.string.OK), null);
+                alert.show();
+            }
+        });
+
+        // Select photo from gallery
+        actForRes = new ActivityResultContracts.StartActivityForResult();
+        actResLauncherSelectPhoto = registerForActivityResult(actForRes, result -> {
+            switch (result.getResultCode()) {
+                case RESULT_OK:
+                    Uri selectedImageUri = result.getData().getData();
+                    if (null != selectedImageUri) {
+                        runOnUiThread(() -> imageViewPhoto.setImageURI(selectedImageUri));
+                    }
+                    break;
             }
         });
 
@@ -95,32 +149,39 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent();
             intent.setType("image/*");
             intent.setAction(Intent.ACTION_PICK);
-            if(intent.resolveActivity(getPackageManager()) != null) {
+            if (intent.resolveActivity(getPackageManager()) != null) {
                 try {
                     actResLauncherSelectPhoto.launch(intent);
                 } catch (Exception e) {
                     Toast.makeText(this, "Permission denied",
                             Toast.LENGTH_SHORT).show();
                 }
-            } else {
-                Toast.makeText(this, "There is no app that support this action",
-                        Toast.LENGTH_SHORT).show();
+            }else {
+                AlertDialog.Builder alert = new AlertDialog.Builder(this)
+                        .setTitle(getString(R.string.Error))
+                        .setMessage(getString(R.string.There_is_no_app_that_support_this_action))
+                        .setPositiveButton(getString(R.string.OK), null);
+                alert.show();
             }
         });
+    }
 
-        actResLauncherSelectPhoto = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
-            Log.e("Take Photo", "YYYYYY");
-            Log.e("Take Photo", "Result = " + result);
-            switch (result.getResultCode()) {
-                case RESULT_OK:
-                    Log.d("Take photo", "Select photo");
-                    Uri selectedImageUri = result.getData().getData();
-                    if(null != selectedImageUri) {
-                        //update the preview image in the layout
-                        runOnUiThread(() -> imageViewPhoto.setImageURI(selectedImageUri));
-                    }
-                    break;
-            }
-        });
+    private boolean hasCameraPermissions(Context context) {
+        if (context.checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            Log.e("Take Photo", "PERMISSION DENIED");
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean hasCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY)) {
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
     }
 }
